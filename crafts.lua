16b1e549960a46737353b262b39eb1f79b5d4d90
Tanmaya Meher <tanmayameher@users.noreply.github.com>
2017-02-21 16:01:35 +0530

    Update crafts.lua

--- crafts.lua
+++ crafts.lua
@@ -1,1 +1,51 @@
+--CRAFTING RECIPES FOR SCIFI NODES
 
+--4 basic plastic from 9 homedecor plastic sheet
+minetest.register_craft({
+	output = "scifi_nodes:white2 4",
+	recipe = {
+		{"homedecor:plastic_sheeting", "homedecor:plastic_sheeting", "homedecor:plastic_sheeting"},
+		{"homedecor:plastic_sheeting", "homedecor:plastic_sheeting", "homedecor:plastic_sheeting"},
+		{"homedecor:plastic_sheeting", "homedecor:plastic_sheeting", "homedecor:plastic_sheeting"}
+	}
+})
+
+--4 plastic wall from 4 plastic
+minetest.register_craft({
+	output = "scifi_nodes:white 4",
+	recipe = {
+		{"scifi_nodes:white2", "scifi_nodes:white2", ""},
+		{"scifi_nodes:white2", "scifi_nodes:white2", ""},
+		{"", "", ""}
+	}
+})
+
+--4 white tile from 4 plastic
+minetest.register_craft({
+	output = "scifi_nodes:whitetile 4",
+	recipe = {
+		{"scifi_nodes:white2", "", "scifi_nodes:white2"},
+		{"scifi_nodes:white2", "", "scifi_nodes:white2"},
+        {"", "", ""}
+	}
+})
+
+--4 white tile2 from 4 plastic
+minetest.register_craft({
+	output = "scifi_nodes:whitetile 4",
+	recipe = {
+		{"scifi_nodes:white2", "", "scifi_nodes:white2"},
+		{"", "", ""},
+		{"scifi_nodes:white2", "", "scifi_nodes:white2"}
+	}
+})
+
+--4 white octagon  from 4 plastic
+minetest.register_craft({
+	output = "scifi_nodes:whitetile 4",
+	recipe = {
+		{"", "scifi_nodes:white2", ""},
+		{"scifi_nodes:white2", "", "scifi_nodes:white2"},
+		{"", "scifi_nodes:white2", ""}
+	}
+})
